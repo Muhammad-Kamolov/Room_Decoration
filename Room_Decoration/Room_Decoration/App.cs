072de1b241717596a233d0b1d1deddbe206b4220
Muhammad <90419040+Muhammad-Kamolov@users.noreply.github.com>
2023-02-02 11:19:32 +0500

    Add files via upload

---
+++ Room_Decoration/Room_Decoration/App.cs
@@ -0,0 +1,77 @@
+#region Namespaces
+using Autodesk.Revit.UI;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Metal_Rolling;
+#endregion
+
+namespace Room_Decoration
+{
+    internal class App : IExternalApplication
+    {
+        public BitmapImage ConvertToBitmap(Image img, Size size)
+        {
+            img = (Image)(new Bitmap(img, size));
+            using (MemoryStream memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+        public Result OnStartup(UIControlledApplication Application)
+        {
+            string tabName = "Третий Трест_Плагины";
+            string panelName = "Плагин";
+
+            // creating the bitImages
+            Image RoomDecorImg = Properties.Resources.icon_remonts;
+
+            // creating the tab
+            Application.CreateRibbonTab(tabName);
+
+            // creating the panel
+            RibbonPanel materials = Application.CreateRibbonPanel(tabName, panelName);
+
+            // creating the buttons 
+            PushButtonData button1 = new PushButtonData("Room_Decoration", "Отделка Помещений", Assembly.GetExecutingAssembly().Location, typeof(Room_Decoration).FullName);
+            button1.ToolTip = "Отделка Помещений";
+            PushButton btn1 = materials.AddItem(button1) as PushButton;
+
+            // setting image to button
+            btn1.LargeImage = ConvertToBitmap(RoomDecorImg, new Size(32, 32));
+            //btn1.Image = ConvertToBitmap(MaterialsImg, new Size(16, 16));
+
+
+            //
+            //
+            Image MaterialsImg = Properties.Resources.metalRolling;
+
+            // creating the buttons 
+            PushButtonData button2 = new PushButtonData("Metell_Rolling", "Металлопрокат", Assembly.GetExecutingAssembly().Location, typeof(Metal_Rolling_Command).FullName);
+            button1.ToolTip = "Металлопрокат";
+            PushButton btn2 = materials.AddItem(button2) as PushButton;
+
+            // setting image to button
+            btn2.LargeImage = ConvertToBitmap(MaterialsImg, new Size(32, 32));
+            //btn1.Image = ConvertToBitmap(MaterialsImg, new Size(16, 16));
+
+            //
+            //
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Room_Decoration/Room_Decoration/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Room_Decoration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboParameters = new System.Windows.Forms.ComboBox();
+            this.comboCondition = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.comboValue = new System.Windows.Forms.ComboBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.comboLevel = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(89, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Параметр";
+            // 
+            // comboParameters
+            // 
+            this.comboParameters.FormattingEnabled = true;
+            this.comboParameters.Location = new System.Drawing.Point(8, 43);
+            this.comboParameters.Name = "comboParameters";
+            this.comboParameters.Size = new System.Drawing.Size(235, 21);
+            this.comboParameters.TabIndex = 0;
+            this.comboParameters.SelectionChangeCommitted += new System.EventHandler(this.comboParameters_SelectionChangeCommitted);
+            // 
+            // comboCondition
+            // 
+            this.comboCondition.FormattingEnabled = true;
+            this.comboCondition.Location = new System.Drawing.Point(290, 43);
+            this.comboCondition.Name = "comboCondition";
+            this.comboCondition.Size = new System.Drawing.Size(131, 21);
+            this.comboCondition.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label2.Location = new System.Drawing.Point(324, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Условие";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.Location = new System.Drawing.Point(540, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Значение";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(540, 425);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "Генерация";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // comboValue
+            // 
+            this.comboValue.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboValue.FormattingEnabled = true;
+            this.comboValue.Location = new System.Drawing.Point(471, 43);
+            this.comboValue.Name = "comboValue";
+            this.comboValue.Size = new System.Drawing.Size(210, 21);
+            this.comboValue.TabIndex = 3;
+            this.comboValue.Enter += new System.EventHandler(this.comboValue_Enter);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(633, 425);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // comboLevel
+            // 
+            this.comboLevel.FormattingEnabled = true;
+            this.comboLevel.Location = new System.Drawing.Point(8, 35);
+            this.comboLevel.Name = "comboLevel";
+            this.comboLevel.Size = new System.Drawing.Size(235, 21);
+            this.comboLevel.TabIndex = 1;
+            this.comboLevel.SelectionChangeCommitted += new System.EventHandler(this.comboLevel_SelectionChangeCommitted);
+            this.comboLevel.Enter += new System.EventHandler(this.comboLevel_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(340, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Уровень";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboValue);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboCondition);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboParameters);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 216);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(691, 91);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboLevel);
+            this.groupBox2.Location = new System.Drawing.Point(252, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 72);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(288, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Параметр и значения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 466);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonOK);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Селектор элементов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboParameters;
+        private System.Windows.Forms.ComboBox comboCondition;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ComboBox comboValue;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ComboBox comboLevel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
